e82c7974c2cbb4c15004d0e4808b09eeb35cdba7
Arbab Nazar <arbab@goicon.com>
2023-02-10 12:40:38 +0500

    enable ssh

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const _ = require("lodash");
 console.log('process.env.DATABASE_URL --------- ', process.env.DATABASE_URL);
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
