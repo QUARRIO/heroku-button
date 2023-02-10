0585f17d47b8643a5140ce28bad8ce149e2019c1
Usama Khan <usamashafqat99@gmail.com>
2023-02-10 20:33:59 +0500

    db initiate

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const http = require("http");
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 
-// const { initiatDatabase } = require("./config/pg");
+const { initiatDatabase } = require("./config/pg");
 app.get("/", (req, res) => {
   res.sendFile("views/root.html", { root: __dirname });
 });
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
-  //   initiatDatabase();
+  initiatDatabase();
 });
