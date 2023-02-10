02e5e4789ac28f7fc0e97439de105ec95403c1ef
Usama Khan <usamashafqat99@gmail.com>
2023-02-10 20:31:26 +0500

    port issue

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const http = require("http");
 const express = require("express");
-const hostname = "127.0.0.1";
-const port = 3000;
+// const port = 3000;
 const app = express();
 
-const { initiatDatabase } = require("./config/pg");
+// const { initiatDatabase } = require("./config/pg");
 app.get("/", (req, res) => {
   res.sendFile("views/root.html", { root: __dirname });
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-  initiatDatabase();
+  console.log(`app listening on port ${process.env.PORT}`);
+  //   initiatDatabase();
 });
